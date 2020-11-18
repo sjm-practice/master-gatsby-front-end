9b753e9ddbbe8d01943f5b8a3267136efea14363
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-11-18 16:19:16 -0500

    add initial pages

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+  <div>
+    <p>404 - Page Not Found.</p>
+  </div>
+);
+
+export default NotFoundPage;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div>
+    <p>Hey! I'm the home page, yo.</p>
+  </div>
+);
+
+export default HomePage;
