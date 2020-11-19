52ddd09201abf3d65a8fc950583738c5af2ccc41
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-11-19 16:22:29 -0500

    add global styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import Nav from './Nav';
 import Footer from './Footer';
+import GlobalStyles from '../styles/GlobalStyles';
+import 'normalize.css';
 
 const Layout = ({ children }) => (
   <div>
+    <GlobalStyles />
     <Nav />
-
     {children}
-
     <Footer />
   </div>
 );
