a2bbc609a50a090bb13de15352bbda88ab501f79
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-11-19 16:29:55 -0500

    add global typography styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,11 +3,14 @@ import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 import 'normalize.css';
 
 const Layout = ({ children }) => (
   <div>
     <GlobalStyles />
+    <Typography />
+
     <Nav />
     {children}
     <Footer />
