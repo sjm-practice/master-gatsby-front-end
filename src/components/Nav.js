d4aac11fbf7c342dcb8f74dc1ca1d2346cca8224
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-11-19 17:05:02 -0500

    style Nav, add Logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,59 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+
+  .logo {
+    transform: translateY(-25%);
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    list-style: none;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+    margin-top: -6rem;
+  }
+
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: 1deg;
+    }
+    &:nth-child(4) {
+      --rotate: 1deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    display: block;
+    &:hover {
+      color: var(--red);
+    }
+  }
+`;
 
 const Nav = () => (
-  <nav>
+  <NavStyles>
     <ul>
       <li>
         <Link to="/">Home</Link>
@@ -11,7 +62,9 @@ const Nav = () => (
         <Link to="/pizzas">Pizza Menu</Link>
       </li>
       <li>
-        <Link to="/">LOGO</Link>
+        <Link to="/">
+          <Logo />
+        </Link>
       </li>
       <li>
         <Link to="/">SliceMasters</Link>
@@ -20,7 +73,7 @@ const Nav = () => (
         <Link to="/">Order Ahead!</Link>
       </li>
     </ul>
-  </nav>
+  </NavStyles>
 );
 
 export default Nav;
