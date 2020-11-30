1091b04a7b2c1e9cdc9b94d7365fcb02a19cefd1
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-11-30 13:50:58 -0500

    configure gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
   siteMetadata: {
     title: 'Slicks Slices',
     siteUrl: 'https://gatsby.pizza',
     description: 'The best pizza place in Hamilton!',
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'qkbsslmt',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
