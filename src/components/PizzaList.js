a78fb049e0be4920a09419704bffc922e24e077b
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-11-30 16:18:27 -0500

    setup pizza grid

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const PizzaGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+  // take your row sizing not from the PizzaStyles div, but from PizzaGridStyles grid
+  grid-template-rows: subgrid;
+
+  // chrome does not support subgrid
+  @supports not (grid-template-rows: subgrid) {
+    grid-template-rows: auto auto 1fr;
+  }
+
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 const SinglePizza = ({ pizza }) => (
-  <div>
+  <PizzaStyles>
     <Link to={`/pizza/${pizza.slug.current}`}>
       <h2>
         <span className="mark">{pizza.name}</span>
       </h2>
-      <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
-      <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
     </Link>
-  </div>
+    <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+    <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
+  </PizzaStyles>
 );
 
 const PizzaList = ({ pizzas }) => {
@@ -19,11 +45,11 @@ const PizzaList = ({ pizzas }) => {
   let x;
 
   return (
-    <div>
+    <PizzaGridStyles>
       {pizzas.map((pizza) => (
         <SinglePizza key={pizza.id} pizza={pizza} />
       ))}
-    </div>
+    </PizzaGridStyles>
   );
 };
 
